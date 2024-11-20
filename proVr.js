5287c1a28a8028366429df29216e441a8fd5fdb0
Keres1232 <162938616+Keres1232@users.noreply.github.com>
2024-11-19 20:17:56 -0500

    color 16bdd5

--- proVr.js
+++ proVr.js
@@ -137,7 +137,7 @@ class Enemy {
     constructor(scene, position = new THREE.Vector3(), speed = 0.05, maxDistance = 2) {
         // Crear geometría y material para el enemigo
         const geometry = new THREE.SphereGeometry(0.5, 16, 16);
-        const material = new THREE.MeshPhongMaterial({ color: 0xFF0000 });
+        const material = new THREE.MeshPhongMaterial({ color: 0x16bdd5 });
         this.mesh = new THREE.Mesh(geometry, material);
 
         // Posicionar el enemigo
